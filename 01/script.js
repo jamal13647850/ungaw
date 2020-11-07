2b5df6786e68dbe6fbadffa2cd130dd23a6f0b23
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2020-11-07 19:22:18 +0330

    Add balls and some changes

--- 01/script.js
+++ 01/script.js
@@ -2,33 +2,23 @@
 window.addEventListener('DOMContentLoaded', (event) => {
         
     //Declare all Variables and grab DOM elements using jQuery
-    let btnMoveRigth = document.getElementsByClassName('btnMoveRigth')[0],
-    btnMoveLeft = document.getElementsByClassName('btnMoveLeft')[0];
-    btnMoveBottom = document.getElementsByClassName('btnMoveBottom')[0];
-    btnMoveTop = document.getElementsByClassName('btnMoveTop')[0];
+    let btnMove = document.getElementsByClassName('btnMove')[0],
     btnChangeColor = document.getElementsByClassName('btnChangeColor')[0];
     btnRotation = document.getElementsByClassName('btnRotation')[0];
     btnChangeScale = document.getElementsByClassName('btnChangeScale')[0];
+    btnBalls = document.getElementsByClassName('btnBalls')[0];
     let tl = new TimelineLite();
             
 
-    btnMoveRigth.addEventListener("click", ()=>{
-        tl.to("#rectangle", 1, {x: 200})
+
+    btnMove.addEventListener("click", ()=>{
+        tl.to("#rectangle", 1, {x: 200});
+        tl.to("#rectangle", {duration: 1, y: 200});
+        tl.to("#rectangle", 1, {x: 0});
+        tl.to("#rectangle", {duration: 1, y: 0});
     });
 
-    btnMoveLeft.addEventListener("click", ()=>{
-        tl.to("#rectangle", 1, {x: 0})
-    });
-
-    btnMoveTop.addEventListener("click", ()=>{
-        tl.to("#rectangle", {duration: 1, y: 0})
-    });
-
-    btnMoveBottom.addEventListener("click", ()=>{
-        tl.to("#rectangle", {duration: 1, y: 200})
-        
-    });
-    
+ 
     btnChangeColor.addEventListener("click", ()=>{
         TweenLite.to("#rectangle", 2, {
             color:"#000000",
@@ -43,4 +33,44 @@ window.addEventListener('DOMContentLoaded', (event) => {
     btnChangeScale.addEventListener("click", ()=>{
         tl.to("#rectangle", {duration: 1, scale: 2});
     });
+    
+    btnBalls.addEventListener("click", ()=>{
+        gsap
+        .timeline({
+            defaults: {
+            duration: 1.5
+            }
+        })
+        .to('.ball', {
+            opacity: 1
+        })
+        .to('.ball2', {
+            opacity: 1
+        })
+        .to('.ball3', {
+            opacity: 1
+        })
+        .to('.ball', {
+            rotationX: 360,
+            rotationY: 180,
+            x: 300,
+            scale: 2,
+            ease: "bounce"
+        })
+        .to('.ball2', {
+            x: 300,
+            scale: 2,
+            ease: "bounce"
+        })
+        .to('.ball3', {
+            x: 300,
+            scale: 2,
+            ease: "bounce"
+        })
+        
+    });
+
+
+
+
 });
